0f93616ded61ed4309a27c3cffac7f81af3fd0ba
jofacabreracoc-afk <jofa.cabrera.coc@phinmaed.com>
2025-08-16 14:43:27 +0800

    Pseudocode: brushing teeth

---
+++ def brush_teeth():.py
@@ -0,0 +1,31 @@
+def brush_teeth():
+
+	cup_of_water = True
+	toothbrush = True
+
+
+	have_toothpaste = False
+
+
+	if have_toothpaste:
+		toothpaste = True
+		print("Toothpaste added to toothbrush.")
+	else:
+		toothpaste = True
+		print("Bought toothpaste and added to toothbrush.")
+
+
+	print("Brushing teeth...")
+
+
+	print("Gargling water...")
+
+
+	print("Spitting water...")
+
+
+	towel = True
+    print("Wiping mouth with towel.")
+
+
+brush_teeth()
